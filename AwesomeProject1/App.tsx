3145d261ede4331d5aed33f4698229220f884024
maybebe7 <74781568+maybebe7@users.noreply.github.com>
2023-04-30 19:57:05 +0200

    realm running

--- AwesomeProject1/App.tsx
+++ AwesomeProject1/App.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import App from './src';
+import Realm from 'realm';
+import {createRealmContext} from '@realm/react';
+//import Programs from './src/schema/ProgramsSchema.js'
+
+class Programs extends Realm.Object {
+  static schema = {
+    name: 'Programs',
+    properties: {
+      _id: 'objectId',
+      name: 'string',
+    },
+    primaryKey: '_id',
+  };
+}
 
 const MainApp = () => {
-  return <App />;
+
+// Create a configuration object
+ const realmConfig = {
+   schema: [Programs],
+ };
+// Create a realm context
+ const {RealmProvider, useRealm, useObject, useQuery} =
+   createRealmContext(realmConfig);
+
+
+  return (
+      <RealmProvider>
+        <App />
+      </RealmProvider>
+    );
+
 };
+
 
 export default MainApp;
